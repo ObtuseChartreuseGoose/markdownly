2b713609e2e8d99ec5c1918b989067a07a8c28d9
Dirt Deodara <thecrowleaps@gmail.com>
2019-08-28 15:09:49 -0700

    created a container for Document

---
+++ src/containers/DocumentsContainer.js
@@ -0,0 +1,9 @@
+import { connect } from 'react-redux';
+import Document from '../components/markdown/Document';
+import { getMarkdown } from '../selectors/markdownSelectors';
+
+const mapStateToProps = state => ({
+  markdown: getMarkdown(state)
+});
+
+export default connect(mapStateToProps)(Document);
