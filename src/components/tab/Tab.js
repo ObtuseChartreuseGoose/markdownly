b706e40c7dfe540fe25716a612202256228f32c1
Dirt Deodara <thecrowleaps@gmail.com>
2019-08-29 10:14:00 -0700

    made change to loadtab button handler per Ryan suggestion

--- src/components/tab/Tab.js
+++ src/components/tab/Tab.js
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types';
 function Tab({ title, id, handleClick }) {
   return (
     <section>
-      <button name="loadTab" onClick={handleClick}>{title}</button>
+      <button name="loadTab" onClick={event => handleClick(event, id)}>{title}</button>
       <button name="deleteTab" onClick={event => handleClick(event, id)}>X</button>
     </section>
   );
