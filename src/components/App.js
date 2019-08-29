eee3e5be3efd4ee06684f549a468a133f574ddab
Dirt Deodara <thecrowleaps@gmail.com>
2019-08-29 11:53:54 -0700

    brought in TabsCpontainer to App

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import DocumentsContainer from '../containers/DocumentsContainer';
+import TabsContainer from '../containers/tab/TabsContainer';
 
 export default function App() {
   return (
     <>
       <h1>Markdown Editor</h1>
+      <TabsContainer />
       <DocumentsContainer />
     </>
   );
